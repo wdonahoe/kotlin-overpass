bf2b3f0958d2870b1befd04e28447a167f920f1a
Wallace Donahoe <wallace.donahoe@rgi-corp.com>
2021-08-28 17:03:12 -0400

    gradle init

---
+++ build.gradle.kts
@@ -0,0 +1,14 @@
+plugins {
+    kotlin("jvm") version "1.5.10"
+}
+
+group = "com.github.wdonahoe.kotlinoverpass"
+version = "0.0.1"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(kotlin("stdlib"))
+}
